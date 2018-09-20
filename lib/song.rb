e3966b79c423995b40b89fe226d3a429ab0aed51
tomgiagtz <tom.giagtzoglou@gmail.com>
2018-09-20 10:24:13 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,75 @@
 class Song
-  attr_accessor :name, :artist_name
-  @@all = []
+	attr_accessor :name, :artist_name
+	@@all = []
 
-  def self.all
-    @@all
-  end
+	def self.all
+		@@all
+	end
 
-  def save
-    self.class.all << self
-  end
+	def save
+		self.class.all << self
+	end
+
+	def self.create
+		song = self.new
+		@@all << song
+		song
+	end
+
+	def self.new_by_name(name)
+		song = self.new
+		song.name = name
+
+		song
+	end
+
+	def self.create_by_name(name)
+		song = self.create
+		song.name = name
+
+		song
+	end
+
+	def self.find_by_name(name)
+		@@all.find { |song| song.name == name }
+	end
+
+	def self.find_or_create_by_name(name)
+		song = find_by_name(name)
+		if song
+			song
+		else
+			self.create_by_name(name)
+		end
+	end
+
+	def self.alphabetical
+		@@all.sort_by { |song| song.name }
+	end
+
+	def self.new_from_filename(filename)
+		song_info = filename.split(" - ")
+		artist = song_info[0]
+		song_name = song_info[1].sub(".mp3", "")
+
+		song = Song.new
+		song.name = song_name
+		song.artist_name = artist
+
+		song
+	end
+
+	def self.create_from_filename(filename)
+		song = new_from_filename(filename)
+		@@all << song
+		song
+	end
+
+	def self.destroy_all
+		@@all.clear
+		
+	end
+
+
 
 end
